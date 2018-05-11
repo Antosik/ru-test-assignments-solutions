fe50bc8a4e5d1aa67f2faa5bb754182c2f1c0288
Antosik <grig_anton96@mail.ru>
2018-05-11 16:32:38 +0300

    [tutu-ru][add] practical2: very base filter

--- tutu-ru/js/table.js
+++ tutu-ru/js/table.js
@@ -3,6 +3,7 @@ class DataTable {
         this.container = container;
         this.page = 0;
         this.sortedBy = new Map(DataTable.headings.map(item => [item, "none" /* none */]));
+        this.filteredBy = new Map(DataTable.headings.map(item => [item, ""]));
         this.renderLoading();
         fetch(dataSource)
             .then(response => response.json())
@@ -10,6 +11,45 @@ class DataTable {
             this.data = response;
             this.renderTable();
         });
+    }
+    // Sort data
+    sortData(parameter, order) {
+        if (DataTable.headings.indexOf(parameter) === -1)
+            return;
+        this.renderLoading();
+        if (order === "asc" /* asc */) {
+            this.data = this.data.sort((a, b) => {
+                if (a[parameter] < b[parameter])
+                    return 1;
+                if (a[parameter] > b[parameter])
+                    return -1;
+                return 0;
+            });
+        }
+        else if (order === "desc" /* desc */) {
+            this.data = this.data.sort((a, b) => {
+                if (a[parameter] < b[parameter])
+                    return -1;
+                if (a[parameter] > b[parameter])
+                    return 1;
+                return 0;
+            });
+        }
+        this.page = 0;
+        this.sortedBy.set(parameter, order);
+        this.renderTable();
+    }
+    // Filter data
+    filterData(parameter, value) {
+        if (DataTable.headings.indexOf(parameter) === -1)
+            return;
+        this.filteredData = this.data.slice(0).filter(dataItem => dataItem[parameter]
+            .toString()
+            .toLowerCase()
+            .indexOf(value) >= 0);
+        this.filteredBy.set(parameter, value);
+        this.page = 0;
+        this.renderTable();
     }
     renderLoading() {
         this.container.innerHTML = "";
@@ -22,11 +62,13 @@ class DataTable {
         this.container.innerHTML = "";
         const table = document.createElement("table");
         table.setAttribute("class", "datatable");
-        const heading = this.getHeading();
+        const heading = this.getHeader();
         table.appendChild(heading);
         const tbody = document.createElement("tbody");
         tbody.setAttribute("class", "datatable__body");
-        const data = this.data.slice(this.page * DataTable.MAX_ROWS_ON_PAGE, (this.page + 1) * DataTable.MAX_ROWS_ON_PAGE);
+        const data = Array.from(this.filteredBy.values()).filter(el => Boolean(el)).length > 0
+            ? this.filteredData.slice(this.page * DataTable.MAX_ROWS_ON_PAGE, (this.page + 1) * DataTable.MAX_ROWS_ON_PAGE)
+            : this.data.slice(this.page * DataTable.MAX_ROWS_ON_PAGE, (this.page + 1) * DataTable.MAX_ROWS_ON_PAGE);
         for (let dataItem of data) {
             const tr = document.createElement("tr");
             for (let heading of DataTable.headings) {
@@ -41,9 +83,17 @@ class DataTable {
         table.appendChild(footer);
         this.container.appendChild(table);
     }
-    getHeading() {
+    getHeader() {
         const thead = document.createElement("thead");
         thead.setAttribute("class", "datatable__head");
+        const headingRow = this.getHeading();
+        const filteringRow = this.getFilters();
+        thead.appendChild(headingRow);
+        thead.appendChild(filteringRow);
+        return thead;
+    }
+    getHeading() {
+        const tr = document.createElement("tr");
         for (let heading of DataTable.headings) {
             const th = document.createElement("th");
             th.innerText = heading;
@@ -71,9 +121,25 @@ class DataTable {
                     : "asc" /* asc */);
             });
             th.appendChild(sortButton);
-            thead.appendChild(th);
+            tr.appendChild(th);
         }
-        return thead;
+        return tr;
+    }
+    getFilters() {
+        const tr = document.createElement("tr");
+        for (let heading of DataTable.headings) {
+            const td = document.createElement("td");
+            const input = document.createElement("input");
+            input.setAttribute("type", "text");
+            input.setAttribute("placeholder", `Filter by ${heading}`);
+            input.setAttribute("value", this.filteredBy.get(heading));
+            input.addEventListener("input", e => {
+                this.filterData(heading, input.value);
+            });
+            td.appendChild(input);
+            tr.appendChild(td);
+        }
+        return tr;
     }
     getFooter() {
         const tfoot = document.createElement("tfoot");
@@ -88,7 +154,9 @@ class DataTable {
         return tfoot;
     }
     getPagination() {
-        const count = Math.ceil(this.data.length / DataTable.MAX_ROWS_ON_PAGE);
+        const count = Array.from(this.filteredBy.values()).filter(el => Boolean(el)).length > 0
+            ? Math.ceil(this.filteredData.length / DataTable.MAX_ROWS_ON_PAGE)
+            : Math.ceil(this.data.length / DataTable.MAX_ROWS_ON_PAGE);
         const nav = document.createElement("nav");
         nav.setAttribute("class", "datatable__pagination");
         const ul = document.createElement("ul");
@@ -110,32 +178,6 @@ class DataTable {
         }
         nav.appendChild(ul);
         return nav;
-    }
-    sortData(parameter, order) {
-        if (DataTable.headings.indexOf(parameter) === -1)
-            return;
-        this.renderLoading();
-        if (order === "asc" /* asc */) {
-            this.data = this.data.sort((a, b) => {
-                if (a[parameter] < b[parameter])
-                    return 1;
-                if (a[parameter] > b[parameter])
-                    return -1;
-                return 0;
-            });
-        }
-        else if (order === "desc" /* desc */) {
-            this.data = this.data.sort((a, b) => {
-                if (a[parameter] < b[parameter])
-                    return -1;
-                if (a[parameter] > b[parameter])
-                    return 1;
-                return 0;
-            });
-        }
-        this.page = 0;
-        this.sortedBy.set(parameter, order);
-        this.renderTable();
     }
 }
 DataTable.MAX_ROWS_ON_PAGE = 50;
--- tutu-ru/js/table.ts
+++ tutu-ru/js/table.ts
@@ -24,15 +24,20 @@ class DataTable {
   private static headings = ["id", "firstName", "lastName", "email", "phone"];
 
   private data: IDataItem[];
+  private filteredData: IDataItem[];
   private page: number;
   private container: Element;
   private sortedBy: Map<string, orderDirection>;
+  private filteredBy: Map<string, string>;
 
   constructor(container: Element, dataSource: string) {
     this.container = container;
     this.page = 0;
     this.sortedBy = new Map(<[string, orderDirection][]>DataTable.headings.map(
       item => [item, orderDirection.none]
+    ));
+    this.filteredBy = new Map(<[string, string][]>DataTable.headings.map(
+      item => [item, ""]
     ));
 
     this.renderLoading();
@@ -42,6 +47,50 @@ class DataTable {
         this.data = response;
         this.renderTable();
       });
+  }
+
+  // Sort data
+  private sortData(parameter: string, order: orderDirection) {
+    if (DataTable.headings.indexOf(parameter) === -1) return;
+
+    this.renderLoading();
+
+    if (order === orderDirection.asc) {
+      this.data = this.data.sort((a, b) => {
+        if (a[parameter] < b[parameter]) return 1;
+        if (a[parameter] > b[parameter]) return -1;
+        return 0;
+      });
+    } else if (order === orderDirection.desc) {
+      this.data = this.data.sort((a, b) => {
+        if (a[parameter] < b[parameter]) return -1;
+        if (a[parameter] > b[parameter]) return 1;
+        return 0;
+      });
+    }
+
+    this.page = 0;
+    this.sortedBy.set(parameter, order);
+
+    this.renderTable();
+  }
+
+  // Filter data
+  private filterData(parameter: string, value: string) {
+    if (DataTable.headings.indexOf(parameter) === -1) return;
+
+    this.filteredData = this.data.slice(0).filter(
+      dataItem =>
+        dataItem[parameter]
+          .toString()
+          .toLowerCase()
+          .indexOf(value) >= 0
+    );
+    this.filteredBy.set(parameter, value);
+
+    this.page = 0;
+
+    this.renderTable();
   }
 
   private renderLoading() {
@@ -60,16 +109,22 @@ class DataTable {
     const table = document.createElement("table");
     table.setAttribute("class", "datatable");
 
-    const heading = this.getHeading();
+    const heading = this.getHeader();
     table.appendChild(heading);
 
     const tbody = document.createElement("tbody");
     tbody.setAttribute("class", "datatable__body");
 
-    const data = this.data.slice(
-      this.page * DataTable.MAX_ROWS_ON_PAGE,
-      (this.page + 1) * DataTable.MAX_ROWS_ON_PAGE
-    );
+    const data =
+      Array.from(this.filteredBy.values()).filter(el => Boolean(el)).length > 0
+        ? this.filteredData.slice(
+            this.page * DataTable.MAX_ROWS_ON_PAGE,
+            (this.page + 1) * DataTable.MAX_ROWS_ON_PAGE
+          )
+        : this.data.slice(
+            this.page * DataTable.MAX_ROWS_ON_PAGE,
+            (this.page + 1) * DataTable.MAX_ROWS_ON_PAGE
+          );
 
     for (let dataItem of data) {
       const tr = document.createElement("tr");
@@ -91,9 +146,21 @@ class DataTable {
     this.container.appendChild(table);
   }
 
-  private getHeading(): Element {
+  private getHeader(): Element {
     const thead = document.createElement("thead");
     thead.setAttribute("class", "datatable__head");
+
+    const headingRow = this.getHeading();
+    const filteringRow = this.getFilters();
+
+    thead.appendChild(headingRow);
+    thead.appendChild(filteringRow);
+
+    return thead;
+  }
+
+  private getHeading(): Element {
+    const tr = document.createElement("tr");
 
     for (let heading of DataTable.headings) {
       const th = document.createElement("th");
@@ -137,10 +204,32 @@ class DataTable {
 
       th.appendChild(sortButton);
 
-      thead.appendChild(th);
+      tr.appendChild(th);
     }
 
-    return thead;
+    return tr;
+  }
+
+  private getFilters(): Element {
+    const tr = document.createElement("tr");
+
+    for (let heading of DataTable.headings) {
+      const td = document.createElement("td");
+
+      const input = document.createElement("input");
+      input.setAttribute("type", "text");
+      input.setAttribute("placeholder", `Filter by ${heading}`);
+      input.setAttribute("value", this.filteredBy.get(heading));
+      input.addEventListener("input", e => {
+        this.filterData(heading, input.value);
+      });
+
+      td.appendChild(input);
+
+      tr.appendChild(td);
+    }
+
+    return tr;
   }
 
   private getFooter(): Element {
@@ -164,7 +253,10 @@ class DataTable {
   }
 
   private getPagination(): Element {
-    const count = Math.ceil(this.data.length / DataTable.MAX_ROWS_ON_PAGE);
+    const count =
+      Array.from(this.filteredBy.values()).filter(el => Boolean(el)).length > 0
+        ? Math.ceil(this.filteredData.length / DataTable.MAX_ROWS_ON_PAGE)
+        : Math.ceil(this.data.length / DataTable.MAX_ROWS_ON_PAGE);
     const nav = document.createElement("nav");
     nav.setAttribute("class", "datatable__pagination");
 
@@ -197,30 +289,5 @@ class DataTable {
     nav.appendChild(ul);
 
     return nav;
-  }
-
-  private sortData(parameter: string, order: orderDirection) {
-    if (DataTable.headings.indexOf(parameter) === -1) return;
-
-    this.renderLoading();
-
-    if (order === orderDirection.asc) {
-      this.data = this.data.sort((a, b) => {
-        if (a[parameter] < b[parameter]) return 1;
-        if (a[parameter] > b[parameter]) return -1;
-        return 0;
-      });
-    } else if (order === orderDirection.desc) {
-      this.data = this.data.sort((a, b) => {
-        if (a[parameter] < b[parameter]) return -1;
-        if (a[parameter] > b[parameter]) return 1;
-        return 0;
-      });
-    }
-
-    this.page = 0;
-    this.sortedBy.set(parameter, order);
-
-    this.renderTable();
   }
 }
